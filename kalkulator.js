b32f8fb93aeb5f842f0653c50ef990c0f26f4d60
Yoga Andika <yogaandhika14@gmail.com>
2020-09-09 22:31:34 +0700

    kalkulator versi javascript

---
+++ kalkulator.js
@@ -0,0 +1,21 @@
+var nilai1 = prompt('Masukan Nilai Pertama')
+var nilai2 = prompt('Masukan Nilai Kedua')
+var op = prompt('-,+,*,/')
+var nilai1int1 = parseInt(nilai1)
+var nilai1int2 = parseInt(nilai2)
+var tambah = nilai1int1 + nilai1int2
+var kurang = nilai1int1 - nilai1int2
+var kali = nilai1int1 * nilai1int2
+var bagi = nilai1int1 / nilai1int2
+
+if (op == '+') {
+    document.write('hasil dari tambah = ' + tambah)
+} else if (op == '-') {
+    document.write('hasil dari kurang = ' + kurang)
+} else if (op == '*') {
+    document.write('hasil dari kali = ' + kali)
+} else if (op == '/') {
+    document.write('hasil dari tambah = ' + bagi)
+} else {
+    document.write('Angka ini salah')
+}
